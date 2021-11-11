77e14c7c3a21e09b65cbc1861152a9c8410444dd
Venom <67759177+nandhuachu@users.noreply.github.com>
2021-11-11 23:44:11 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -17,7 +17,6 @@ ME_MSG = """
 <b>Unfinished Guess :</b> <code>{}</code>
 <b>Total Questions :</b> <code>{}</code>
 """
-HELP_TXT ="""hi"""
 GITHUB_URL = "https://t.me/tvseriezzz"
 DEV_URL = "https://t.me/MrC_VENOM"
 
